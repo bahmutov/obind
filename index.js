ebad5ba52939d689bb9ca878eb01e1396d9b452d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-10-17 09:15:56 -0400

    controlled export node

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-function copy(obj) {
-  return JSON.parse(JSON.stringify(obj));
-}
-
-function isPlainObject(x) {
-  return typeof x === 'object' &&
-    Object.prototype.toString.call(x) === '[object Object]';
-}
-
-function obind(fn, partial) {
-  if (!isPlainObject(partial)) {
-    throw new Error('Expected plain object partial argument');
+;(function initObind() {
+  function copy(obj) {
+    return JSON.parse(JSON.stringify(obj));
   }
-  var obj = copy(partial);
 
-  return function obound(arg) {
-    if (arguments.length === 1 &&
-      isPlainObject(arg)) {
-      Object.keys(arg).forEach(function (key) {
-        if (typeof copy[key] === 'undefined') {
-          obj[key] = arg[key];
-        }
-      });
-      return fn(obj);
-    } else if (isPlainObject(partial)) {
-      return fn(partial);
-    } else {
-      return fn(arg);
+  function isPlainObject(x) {
+    return typeof x === 'object' &&
+      Object.prototype.toString.call(x) === '[object Object]';
+  }
+
+  function obind(fn, partial) {
+    if (!isPlainObject(partial)) {
+      throw new Error('Expected plain object partial argument');
     }
-  };
-}
+    var obj = copy(partial);
 
-module.exports = obind;
+    return function obound(arg) {
+      if (arguments.length === 1 &&
+        isPlainObject(arg)) {
+        Object.keys(arg).forEach(function (key) {
+          if (typeof copy[key] === 'undefined') {
+            obj[key] = arg[key];
+          }
+        });
+        return fn(obj);
+      } else if (isPlainObject(partial)) {
+        return fn(partial);
+      } else {
+        return fn(arg);
+      }
+    };
+  }
+
+  if (typeof module === 'object') {
+    module.exports = obind;
+  }
+}());
