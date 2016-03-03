0d91f15ecbf775d87c731732dec81da9cf3c4673
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-03-03 08:17:43 -0500

    chore(lint): added automatic linting using standard-format

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-;(function initObind() {
-  function copy(obj) {
-    return JSON.parse(JSON.stringify(obj));
+;(function initObind () {
+  function copy (obj) {
+    return JSON.parse(JSON.stringify(obj))
   }
 
-  function isFunction(x) {
-    return typeof x === 'function';
+  function isFunction (x) {
+    return typeof x === 'function'
   }
 
-  function isPlainObject(x) {
+  function isPlainObject (x) {
     return typeof x === 'object' &&
-      Object.prototype.toString.call(x) === '[object Object]';
+    Object.prototype.toString.call(x) === '[object Object]'
   }
 
-  function obind(fn, partial) {
+  function obind (fn, partial) {
     if (!isFunction(fn)) {
-      throw new Error('Expected function as first argument to obind');
+      throw new Error('Expected function as first argument to obind')
     }
     if (!isPlainObject(partial)) {
-      throw new Error('Expected plain object partial argument');
+      throw new Error('Expected plain object partial argument')
     }
-    var obj = copy(partial);
+    var obj = copy(partial)
 
-    return function obound(arg) {
+    return function obound (arg) {
       if (arguments.length > 0 &&
         isPlainObject(arg)) {
         Object.keys(arg).forEach(function (key) {
           if (typeof copy[key] === 'undefined') {
-            obj[key] = arg[key];
+            obj[key] = arg[key]
           }
-        });
-        var allArguments = Array.prototype.slice.call(arguments, 0);
-        allArguments[0] = obj;
-        return fn.apply(null, allArguments);
+        })
+        var allArguments = Array.prototype.slice.call(arguments, 0)
+        allArguments[0] = obj
+        return fn.apply(null, allArguments)
       } else if (isPlainObject(partial)) {
-        return fn(partial);
+        return fn(partial)
       } else {
-        return fn(arg);
+        return fn(arg)
       }
-    };
+    }
   }
 
   if (typeof module === 'object') {
-    module.exports = obind;
+    module.exports = obind
   }
   if (typeof window === 'object') {
-    window.obind = obind;
+    window.obind = obind
   }
-}());
+}())
--- test/obind-spec.js
+++ test/obind-spec.js
@@ -1,160 +1,161 @@
-var la = require('lazy-ass');
-var check = require('check-more-types');
+const la = require('lazy-ass')
+const check = require('check-more-types')
 
+/* global describe, it */
 describe('obind', function () {
-  var obind = require('..');
-  function foo(opts) {
-    la(check.object(opts), 'expected opts to be an object', opts);
-    la(opts.bar === 'bar', 'expected opts.bar to be "bar", got', opts.bar);
-    la(opts.baz === 'baz', 'expected opts.baz to be "baz", got', opts.baz);
-    la(Object.keys(opts).length === 2, 'expected only bar and baz, got', opts);
-    return 'foo';
+  var obind = require('..')
+  function foo (opts) {
+    la(check.object(opts), 'expected opts to be an object', opts)
+    la(opts.bar === 'bar', 'expected opts.bar to be "bar", got', opts.bar)
+    la(opts.baz === 'baz', 'expected opts.baz to be "baz", got', opts.baz)
+    la(Object.keys(opts).length === 2, 'expected only bar and baz, got', opts)
+    return 'foo'
   }
 
   describe('foo is a test function', function () {
     it('has check function', function () {
-      la(check.fn(foo));
-    });
+      la(check.fn(foo))
+    })
 
     it('needs bar and baz', function () {
       var good = {
         bar: 'bar',
         baz: 'baz'
-      };
-      la(foo(good) === 'foo');
-    });
+      }
+      la(foo(good) === 'foo')
+    })
 
     it('checks bar value', function () {
       var bad = {
         bar: 'bar2',
         baz: 'baz'
-      };
+      }
       la(check.raises(function () {
-        foo(extra);
-      }));
-    });
+        foo(bad)
+      }))
+    })
 
     it('needs nothing more', function () {
       var extra = {
         bar: 'bar',
         baz: 'baz',
         two: 2
-      };
+      }
       la(check.raises(function () {
-        foo(extra);
-      }));
-    });
-  });
+        foo(extra)
+      }))
+    })
+  })
 
   it('is a function', function () {
-    la(check.fn(obind));
-  });
+    la(check.fn(obind))
+  })
 
   it('returns a new function', function () {
-    var withBar = obind(foo, {});
-    la(check.fn(withBar));
-  });
+    var withBar = obind(foo, {})
+    la(check.fn(withBar))
+  })
 
   it('combines old and new args', function () {
-    var withBar = obind(foo, { bar: 'bar' });
-    var result = withBar({ baz: 'baz' });
-    la(result === 'foo', 'correct result', result);
-  });
+    var withBar = obind(foo, { bar: 'bar' })
+    var result = withBar({ baz: 'baz' })
+    la(result === 'foo', 'correct result', result)
+  })
 
   it('can combine empty object', function () {
-    var withEmpty = obind(foo, {});
-    var result = withEmpty({ bar: 'bar', baz: 'baz' });
-    la(result === 'foo', 'correct result', result);
-  });
+    var withEmpty = obind(foo, {})
+    var result = withEmpty({ bar: 'bar', baz: 'baz' })
+    la(result === 'foo', 'correct result', result)
+  })
 
   it('can combine multiple times', function () {
-    var withEmpty = obind(foo, {});
-    var withBar = obind(withEmpty, { bar: 'bar' });
-    var withBarBaz = obind(withBar, { baz: 'baz' });
-    var result = withBarBaz();
-    la(result === 'foo', 'correct result', result);
-  });
+    var withEmpty = obind(foo, {})
+    var withBar = obind(withEmpty, { bar: 'bar' })
+    var withBarBaz = obind(withBar, { baz: 'baz' })
+    var result = withBarBaz()
+    la(result === 'foo', 'correct result', result)
+  })
 
   it('clones initial object', function () {
-    var bar = { bar: 'bar' };
-    var withBar = obind(foo, bar);
-    var result = withBar({ baz: 'baz' });
-    la(result === 'foo', 'initial result', result);
-    bar.bar = 'nope';
-    result = withBar({ baz: 'baz' });
-    la(result === 'foo', 'second result', result);
-  });
-});
+    var bar = { bar: 'bar' }
+    var withBar = obind(foo, bar)
+    var result = withBar({ baz: 'baz' })
+    la(result === 'foo', 'initial result', result)
+    bar.bar = 'nope'
+    result = withBar({ baz: 'baz' })
+    la(result === 'foo', 'second result', result)
+  })
+})
 
 describe('obind examples', function () {
-  var obind = require('..');
+  var obind = require('..')
   it('simple', function () {
-    function add(options) {
-      return options.a + options.b;
+    function add (options) {
+      return options.a + options.b
     }
-    var add2 = obind(add, { a: 2 });
-    var result = add2({ b: 3 });
-    la(result === 5);
-  });
+    var add2 = obind(add, { a: 2 })
+    var result = add2({ b: 3 })
+    la(result === 5)
+  })
 
   it('multiple steps', function () {
-    function add(options) {
-      return options.a + options.b;
+    function add (options) {
+      return options.a + options.b
     }
-    var add2 = obind(add, { a: 2 });
-    var add2to5 = obind(add2, { b: 5 });
-    var result = add2to5();
-    la(result === 7);
-  });
+    var add2 = obind(add, { a: 2 })
+    var add2to5 = obind(add2, { b: 5 })
+    var result = add2to5()
+    la(result === 7)
+  })
 
   it('needs empty object to start with', function () {
-    function add(options) {
-      return options.a + options.b;
+    function add (options) {
+      return options.a + options.b
     }
-    var addNothing = obind(add, {});
-    var result = addNothing({ a: 2, b: 10 });
-    la(result === 12);
-  });
+    var addNothing = obind(add, {})
+    var result = addNothing({ a: 2, b: 10 })
+    la(result === 12)
+  })
 
   it('no changes to bound values', function () {
-    function add(options) {
-      return options.a + options.b;
+    function add (options) {
+      return options.a + options.b
     }
     var options = {
       a: 10,
       b: 20
-    };
-    var add10to20 = obind(add, options);
-    options.a = 1;
-    var result = add10to20({});
+    }
+    var add10to20 = obind(add, options)
+    options.a = 1
+    var result = add10to20({})
     // changing options.a has no effect
-    la(result === 30);
-  });
-});
+    la(result === 30)
+  })
+})
 
 describe('multiple arguments', function () {
-  var obind = require('..');
-  function foo(opts, second, third) {
-    la(check.object(opts), 'expected opts to be an object', opts);
-    la(opts.bar === 'bar', 'expected opts.bar to be "bar", got', opts.bar);
-    la(opts.baz === 'baz', 'expected opts.baz to be "baz", got', opts.baz);
-    la(Object.keys(opts).length === 2, 'expected only bar and baz, got', opts);
+  var obind = require('..')
+  function foo (opts, second, third) {
+    la(check.object(opts), 'expected opts to be an object', opts)
+    la(opts.bar === 'bar', 'expected opts.bar to be "bar", got', opts.bar)
+    la(opts.baz === 'baz', 'expected opts.baz to be "baz", got', opts.baz)
+    la(Object.keys(opts).length === 2, 'expected only bar and baz, got', opts)
 
-    la(second === 'second', 'second arg', second);
-    la(third === 'third', 'third arg', third);
+    la(second === 'second', 'second arg', second)
+    la(third === 'third', 'third arg', third)
 
-    return 'foo';
+    return 'foo'
   }
 
   it('needs second and third arguments too', function () {
     var options = {
       bar: 'bar',
       baz: 'baz'
-    };
-    var bound = obind(foo, options);
-    la(check.fn(bound), 'returned bound function');
+    }
+    var bound = obind(foo, options)
+    la(check.fn(bound), 'returned bound function')
 
-    var result = bound({}, 'second', 'third');
-    la(result === 'foo', 'returned result', result);
-  });
-});
+    var result = bound({}, 'second', 'third')
+    la(result === 'foo', 'returned result', result)
+  })
+})
